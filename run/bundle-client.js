5869a9afe95e6d4d99030f8e912bd4519638831e
e2tha-e <ee@electric-eloquence.io>
2020-06-27 19:52:59 -0400

    prettier promise chaining

--- run/bundle-client.js
+++ run/bundle-client.js
@@ -13,55 +13,59 @@ const prefix = '../';
 const src = 'dist/requerio.npm.js';
 const srcMjs = 'dist/requerio.npm.mjs';
 
-new Promise((resolve) => {
-  const begin = Date.now();
+new Promise(
+  (resolve) => {
+    const begin = Date.now();
 
-  browserify(prefix + src)
-    .transform(babelify, {presets: ['@babel/preset-env']})
-    .bundle((err, buf) => {
-      if (err) {
-        throw err;
-      }
+    browserify(prefix + src)
+      .transform(babelify, {presets: ['@babel/preset-env']})
+      .bundle((err, buf) => {
+        if (err) {
+          throw err;
+        }
 
-      const browserified = buf.toString('utf8');
-      const uglified = uglifyES.minify(browserified);
+        const browserified = buf.toString('utf8');
+        const uglified = uglifyES.minify(browserified);
 
-      if (uglified.error) {
-        throw uglified.error;
-      }
+        if (uglified.error) {
+          throw uglified.error;
+        }
 
-      fs.writeFileSync(prefix + bld, uglified.code);
+        fs.writeFileSync(prefix + bld, uglified.code);
 
-      const end = Date.now();
-      const elapsed = end - begin;
+        const end = Date.now();
+        const elapsed = end - begin;
 
-      console.log();
-      console.log('\x1b[1m\x1b[36m' + src + '\x1b[0m\x1b[36m → \x1b[1m' + bld + '\x1b[0m\x1b[36m' + '...' + '\x1b[0m');
-      console.log('\x1b[32m' + 'created ' + `\x1b[1m${bld} ` + '\x1b[0m\x1b[32m' + 'in \x1b[1m' + `${elapsed}ms` +
-        '\x1b[0m');
+        console.log();
+        console.log('\x1b[1m\x1b[36m' + src + '\x1b[0m\x1b[36m → \x1b[1m' + bld + '\x1b[0m\x1b[36m' + '...' + '\x1b[0m');
+        console.log('\x1b[32m' + 'created ' + `\x1b[1m${bld} ` + '\x1b[0m\x1b[32m' + 'in \x1b[1m' + `${elapsed}ms` +
+          '\x1b[0m');
 
-      resolve();
-    });
-})
-.then(() => {
-  const begin = Date.now();
-  const mjs = fs.readFileSync(prefix + srcMjs, 'utf8');
-  const uglifiedMjs = uglifyES.minify(mjs);
-
-  if (uglifiedMjs.error) {
-    throw uglifiedMjs.error;
+        resolve();
+      });
   }
+).then(
+  () => {
+    const begin = Date.now();
+    const mjs = fs.readFileSync(prefix + srcMjs, 'utf8');
+    const uglifiedMjs = uglifyES.minify(mjs);
 
-  fs.writeFileSync(prefix + bldMjs, uglifiedMjs.code);
+    if (uglifiedMjs.error) {
+      throw uglifiedMjs.error;
+    }
 
-  const end = Date.now();
-  const elapsed = end - begin;
+    fs.writeFileSync(prefix + bldMjs, uglifiedMjs.code);
 
-  console.log();
-  console.log('\x1b[1m\x1b[36m' + srcMjs + '\x1b[0m\x1b[36m → \x1b[1m' + bldMjs + '\x1b[0m\x1b[36m' + '...' + '\x1b[0m');
-  console.log('\x1b[32m' + 'created ' + `\x1b[1m${bldMjs} ` + '\x1b[0m\x1b[32m' + 'in \x1b[1m' + `${elapsed}ms` +
-    '\x1b[0m');
-})
-.catch((err) => {
-  throw err;
-});
+    const end = Date.now();
+    const elapsed = end - begin;
+
+    console.log();
+    console.log('\x1b[1m\x1b[36m' + srcMjs + '\x1b[0m\x1b[36m → \x1b[1m' + bldMjs + '\x1b[0m\x1b[36m' + '...' + '\x1b[0m');
+    console.log('\x1b[32m' + 'created ' + `\x1b[1m${bldMjs} ` + '\x1b[0m\x1b[32m' + 'in \x1b[1m' + `${elapsed}ms` +
+      '\x1b[0m');
+  }
+).catch(
+  (err) => {
+    throw err;
+  }
+);
